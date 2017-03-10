5135c88e5882c96dcbbc9656dd38359b707710c2
Corin Sandford <cosa0481@colorado.edu>
2017-03-10 13:17:09 -0700

    Added methods and variables to classes.

---
+++ OOAD/src/com/project/System.java
@@ -0,0 +1,21 @@
+package com.project;
+
+public class System {
+	
+	static public void search(String searchString) {
+		
+	}
+	
+	static public void onSuccessfulPurchase(Order order) {
+		
+	}
+	
+	static private void generateInvoice() {
+		
+	}
+	
+	// Main function
+	public static void main(String[] args) {
+		
+	}
+}
