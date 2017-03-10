d1ae1c5422d51b652effc838abf64966d5759084
rohit <rohitgupta0810@gmail.com>
2017-03-10 13:35:37 -0700

    more changes

--- OOAD/src/com/project/System.java
+++ OOAD/src/com/project/System.java
@@ -1,21 +1,21 @@
 package com.project;
 
 public class System {
-	
+
 	static public void search(String searchString) {
-		
+
 	}
-	
+
 	static public void onSuccessfulPurchase(Order order) {
-		
+		generateInvoice(order);
 	}
-	
-	static private void generateInvoice() {
-		
+
+	static private void generateInvoice(Order order) {
+
 	}
-	
+
 	// Main function
 	public static void main(String[] args) {
-		
+
 	}
 }
--- OOAD/src/com/project/VenmoDAO.java
+++ OOAD/src/com/project/VenmoDAO.java
@@ -2,4 +2,13 @@ package com.project;
 
 public class VenmoDAO {
 
+	private String venmoUserName;
+
+	public String getVenmoUserName() {
+		return venmoUserName;
+	}
+
+	public void setVenmoUserName(String venmoUserName) {
+		this.venmoUserName = venmoUserName;
+	}
 }
