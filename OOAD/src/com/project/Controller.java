2dc82683c259965e60935f4db0cc2ea351740b14
rohit <rohitgupta0810@gmail.com>
2017-03-13 20:19:23 -0600

    updatng rohit's portion

--- OOAD/src/com/project/Controller.java
+++ OOAD/src/com/project/Controller.java
@@ -2,4 +2,27 @@ package com.project;
 
 public class Controller {
 
+	public void getPlans() {
+
+	}
+
+	public void addToCart(Item membershipPlans) {
+
+	}
+
+	public void checkout() {
+
+	}
+
+	public void fetchPaymentOption() {
+
+	}
+
+	public void processPayment() {
+
+	}
+
+	public void onSuccessfulOrder() {
+
+	}
 }
